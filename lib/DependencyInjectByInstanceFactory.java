ca40ceb6fe684e913f8c614f10438b9e1bc8ddfc
zhangype <1141404898@qq.com>
2016-11-04 16:34:03 +0800

    Delete DependencyInjectByInstanceFactory.java

--- lib/DependencyInjectByInstanceFactory.java
+++
@@ -1,9 +0,0 @@
-package chapter3;
-
-import helloworld.HelloApi;
-
-public class DependencyInjectByInstanceFactory {
-	public HelloApi newInstance(String message, int index) {
-		return new HelloImpl3(message, index);
-	}
-}
